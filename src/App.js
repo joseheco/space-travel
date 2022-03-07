48bd966b6d352ab0385db23f6d4f65abce52cb62
Jose Herrera <jferhc@gmail.com>
2022-03-07 17:45:33 -0500

    add basic structure

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Missionlist from './components/Missions';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/missions" />
+      <Route path="/missions" element={<Missionlist />} />
       <Route path="/profile" />
     </Routes>
   </BrowserRouter>
