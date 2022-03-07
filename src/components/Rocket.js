446e28c966b7a2de79127a603eddfc3ce8e110e3
Gaby <gaby.es.iaa@gmail.com>
2022-03-07 18:52:36 -0500

    Display reserved flag and cancel reservation button when a rocket is reserved, in the Rocket component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,21 +1,43 @@
 import { PropTypes } from 'prop-types';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setRocketReserved } from '../redux/rockets/rocketsReducer';
 
-const Rocket = ({ rocket }) => (
-  <li className="rocket__list-item">
-    <div className="list__item-image">
-      <img src={rocket.flickr_images[0]} alt="rocket" />
-    </div>
-    <div className="list__item-details">
-      <h2>{rocket.rocket_name}</h2>
-      <p>
-        <span className="reserved-flag">Reserved</span>
-        {rocket.description}
-      </p>
-      <button type="button" className="btn-blue btn-active" data-id={rocket.id}>Reserve Rocket</button>
-      <button type="button" className="btn-cancel btn-active" data-id={rocket.id}>Cancel Reservation</button>
-    </div>
-  </li>
-);
+const Rocket = ({ rocket }) => {
+  const [reservedFlagStyle, setResevedFlagStyle] = useState('');
+  const [bookingBtnStyle, setBookingBtnStyle] = useState('btn-blue');
+  const [bookingState, setBookingState] = useState('Reserve Rocket');
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (rocket.reserved) {
+      setResevedFlagStyle('ele-active');
+      setBookingBtnStyle('btn-cancel');
+      setBookingState('Cancel Reservation');
+    }
+  });
+
+  const makeReservation = (e) => {
+    dispatch(setRocketReserved(+e.target.dataset.id));
+  };
+
+  return (
+    <li className="rocket__list-item">
+      <div className="list__item-image">
+        <img src={rocket.flickr_images[0]} alt="rocket" />
+      </div>
+      <div className="list__item-details">
+        <h2>{rocket.rocket_name}</h2>
+        <p>
+          <span className={'reserved-flag '.concat(reservedFlagStyle)}>Reserved</span>
+          {rocket.description}
+        </p>
+        <button type="button" className={bookingBtnStyle} data-id={rocket.id} onClick={makeReservation}>{bookingState}</button>
+      </div>
+    </li>
+  );
+};
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
@@ -23,6 +45,7 @@ Rocket.propTypes = {
     rocket_name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    reserved: PropTypes.bool,
   }).isRequired,
 };
 
