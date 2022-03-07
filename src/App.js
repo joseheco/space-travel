2e17dffc1f6e26a831fef30a0336abec42e62e5a
Gaby <gaby.es.iaa@gmail.com>
2022-03-07 16:53:34 -0500

    Import the new App.scss stylesheet in App.js component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './scss/App.scss';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
