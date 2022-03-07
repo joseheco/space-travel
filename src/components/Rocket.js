ce4f067131fe82ae914a60d819264d8d52f48dea
Gaby <gaby.es.iaa@gmail.com>
2022-03-07 16:42:41 -0500

    Update the Rocket component to render it

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -2,20 +2,24 @@ import { PropTypes } from 'prop-types';
 
 const Rocket = ({ rocket }) => (
   <li className="rocket__list-item">
-    <div className="rocket__image">
+    <div className="list__item-image">
       <img src={rocket.flickr_images[0]} alt="rocket" />
     </div>
-    <div className="rocket__details">
+    <div className="list__item-details">
       <h2>{rocket.rocket_name}</h2>
-      <p>{rocket.description}</p>
-      <button type="button" className="btn">Reserve Rocket</button>
+      <p>
+        <span className="reserved-flag">Reserved</span>
+        {rocket.description}
+      </p>
+      <button type="button" className="btn-blue btn-active" data-id={rocket.id}>Reserve Rocket</button>
+      <button type="button" className="btn-cancel btn-active" data-id={rocket.id}>Cancel Reservation</button>
     </div>
   </li>
 );
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
     rocket_name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
