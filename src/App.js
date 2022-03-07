93d92101c6c7501a81ff6c661d366cdc66c771b7
Jose Herrera <jferhc@gmail.com>
2022-03-07 15:37:10 -0500

    old changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Missions from './components/Missions'
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" />
-      <Route path="/missions" />
+      <Route path="/missions" element={<Missions/>} />
       <Route path="/profile" />
     </Routes>
   </BrowserRouter>
